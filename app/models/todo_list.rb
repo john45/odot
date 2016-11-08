ed7934da3fc93780cbbd0b464f90cdff1e54367c
Adamenko <mangamen45@gmail.com>
2016-11-08 23:12:39 +0200

    add validations for description and title

--- app/models/todo_list.rb
+++ app/models/todo_list.rb
@@ -1,3 +1,4 @@
 class TodoList < ApplicationRecord
   validates :title, presence: true, length: {minimum: 3}
+  validates :body, presence: true, length: {minimum: 5}
 end
--- spec/features/todo_lists/create_spec.rb
+++ spec/features/todo_lists/create_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 describe 'Create todo list' do
+
   it 'Redirect to index file on success' do
     visit "/todo_lists"
     click_link "New Todo List"
@@ -48,6 +49,44 @@ describe 'Create todo list' do
 
     visit "/todo_lists"
     expect(page).to_not have_content("This is what aim todo today!")
+
+  end
+
+  it "have display error when todolist no body " do
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    click_link "New Todo List"
+    expect(page).to have_content("New Todo List")
+
+    fill_in "Title", with: "Grocery list"
+    fill_in "Body", with: ""
+    click_button "Create Todo list"
+
+    expect(page).to have_content("error")
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    expect(page).to_not have_content("Grocery list")
+
+  end
+
+  it "have display error when todolist less 5 characters" do
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    click_link "New Todo List"
+    expect(page).to have_content("New Todo List")
+
+    fill_in "Title", with: "Grocery list"
+    fill_in "Body", with: "body"
+    click_button "Create Todo list"
+
+    expect(page).to have_content("error")
+    expect(TodoList.count).to eq(0)
+
+    visit "/todo_lists"
+    expect(page).to_not have_content("Grocery list")
 
   end
 end
