143b2de322520e82a26a963571733ae15c0b3b2f
Adamenko <mangamen45@gmail.com>
2016-11-13 00:59:58 +0200

    Some clean our text - refactoring

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,15 +1,15 @@
 class TodoItemsController < ApplicationController
+  before_action :find_todo_list
+
   def index
-    @todo_list = TodoList.find(params[:todo_list_id])
+
   end
 
   def new
-    @todo_list = TodoList.find(params[:todo_list_id])
     @todo_item = @todo_list.todo_items.new
   end
 
   def create
-    @todo_list = TodoList.find(params[:todo_list_id])
     @todo_item = @todo_list.todo_items.new(todo_item_params)
     if @todo_item.save
       flash[:success] = "Added todo list item."
@@ -21,13 +21,12 @@ class TodoItemsController < ApplicationController
   end
 
   def edit
-    @todo_list = TodoList.find(params[:todo_list_id])
     @todo_item = @todo_list.todo_items.find(params[:id])
 
   end
 
   def update
-    @todo_list = TodoList.find(params[:todo_list_id])
+
     @todo_item = @todo_list.todo_items.find(params[:id])
 
     if @todo_item.update_attribute(todo_item_params)
@@ -40,12 +39,13 @@ class TodoItemsController < ApplicationController
 
   end
 
-  def url_options
-    { todo_list_id: params[:todo_list_id]}.merge(super)
-
-  end
   private
+
+    def find_todo_list
+      @todo_list = TodoList.find(params[:todo_list_id])
+    end
+
     def todo_item_params
-      params[:todo_item].permit(:content)
+      params[:todo_item].permit(:todo_list_id, :content)
     end
 end
