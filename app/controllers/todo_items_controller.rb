76edaa7ae82e0b4e4bebce2286a4e9617d316155
Adamenko <mangamen45@gmail.com>
2016-11-13 08:49:09 +0200

    Add delete items functional and spec for this

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -39,6 +39,20 @@ class TodoItemsController < ApplicationController
 
   end
 
+  def destroy
+    @todo_item = @todo_list.todo_items.find(params[:id])
+    if @todo_item.destroy
+      flash[:success] = "Todo list item was deleted."
+    else
+      flash[:error] = "Todo list item could not be deleted."
+    end
+    redirect_to todo_list_todo_items_path
+  end
+
+  def url_options
+    { todo_list_id: params[:todo_list_id] }.merge(super)
+  end
+
   private
 
     def find_todo_list
@@ -46,6 +60,6 @@ class TodoItemsController < ApplicationController
     end
 
     def todo_item_params
-      params[:todo_item].permit(:todo_list_id, :content)
+      params[:todo_item].permit(:content)
     end
 end
